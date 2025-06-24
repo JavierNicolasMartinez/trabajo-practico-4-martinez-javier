5878e63158d6b1ec2749dc539367e19ea9192fde
Javier Nicolas Martinez <javierformosanm@gmail.com>
2025-06-23 22:43:50 -0300

    Data base preparada

--- src/config/database.js
+++ src/config/database.js
@@ -2,6 +2,7 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
+//buscado en sequelize.
 export const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
@@ -11,3 +12,12 @@ export const sequelize = new Sequelize(
         dialect: process.env.DB_DIALECT
     },
 );
+
+export const startDB = async()=> {
+    try {
+        await sequelize.authenticate();
+        console.log("La conexion se ha realizado con exito.");
+    } catch (error) {
+        console.error("No se pudo realizar la conexón con la base de datos.");
+    }
+};
