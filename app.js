04f61785d3b60b130e7bfe3b980b0a892e03897e
Javier Nicolas Martinez <javierformosanm@gmail.com>
2025-06-23 23:38:33 -0300

    Servidor corriendo en el PORT especificado.

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+import dotenv  from "dotenv";
 import express from "express";
-
+import characterRoutes from "./src/routes/character.routes.js";
+import {startDB}  from "./src/config/database.js";
+dotenv.config();
 
 
 const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
+app.use("/characters", characterRoutes);
+
+app.listen(PORT, async()=> {
+    await startDB();
+    console.log(`El servidor esta corriendo en: http://localhost:${PORT}/characters`);
+});
+
