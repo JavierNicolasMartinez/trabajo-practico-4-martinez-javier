8bd1bb3018477ccdc500b48588029af2681554f4
Javier Nicolas Martinez <javierformosanm@gmail.com>
2025-06-26 14:08:39 -0300

    Ultimos cambios para entrega, verificación de nombres correctos y consignas completas

--- src/routes/character.routes.js
+++ src/routes/character.routes.js
@@ -2,10 +2,10 @@ import express from "express";
 import { createCharacter,  updateCharacter, obtenerTodosLosPersonajes, obtenerPorId, deleteCharacter } from "../controllers/character.controllers.js";
 const router = express.Router();
 
-router.post("/characters", createCharacter);
-router.delete("/characters/:id", deleteCharacter);
-router.get("/characters/:id", obtenerPorId);
-router.get("/characters", obtenerTodosLosPersonajes);
-router.put("/characters/:id", updateCharacter);
+router.post("/characters", createCharacter); //Crear un nuevo personaje
+router.delete("/characters/:id", deleteCharacter); //Eliminar un personaje
+router.get("/characters/:id", obtenerPorId); //Devolver un personaje por su ID
+router.get("/characters", obtenerTodosLosPersonajes); //Devolver todos los personajes registrados
+router.put("/characters/:id", updateCharacter); //Actualizar los datos de un personaje
 
 export default router;
